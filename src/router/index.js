7e3734a077f3d4365ff3b19abfaf189bb006d0c3
Marcel Haßlinger <me@marhali.de>
2021-07-07 22:39:19 +0200

    feat(plugins): enable vue-head plugin

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import VueHead from 'vue-head'
 import Landing from '@/views/Landing'
 
+Vue.use(VueHead)
 Vue.use(VueRouter)
 
 const routes = [
