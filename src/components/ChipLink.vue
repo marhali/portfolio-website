690ee1e5a1b2bbc64549b4566aea91e792fdf6fb
Marcel Haßlinger <me@marhali.de>
2021-07-07 19:01:40 +0200

    feat(components): add reusable components for reference links

---
+++ src/components/ChipLink.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-chip
+    :to="to"
+    :href="href"
+    :target="href ? '_blank' : ''"
+    rel="noopener noreferrer"
+  >
+    <slot />
+  </v-chip>
+</template>
+
+<script>
+export default {
+  name: 'ChipLink',
+
+  props: ['href', 'to'],
+}
+</script>
+
+<style scoped>
+
+</style>
